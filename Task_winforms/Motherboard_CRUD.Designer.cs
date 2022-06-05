463e1193d55f0fc2d13e762e7aa9d2241d4ee1dc
Nikita-Oskin <91139480+Nikita-Oskin@users.noreply.github.com>
2022-06-05 19:19:05 +0300

    Update Motherboard_CRUD.Designer.cs

--- Task_winforms/Motherboard_CRUD.Designer.cs
+++ Task_winforms/Motherboard_CRUD.Designer.cs
@@ -22,11 +22,7 @@ namespace pcshop
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+            
         private void InitializeComponent()
         {
             this.cbox_m_brand = new System.Windows.Forms.ComboBox();
