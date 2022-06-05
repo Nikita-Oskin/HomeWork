f736df942fdfb422d0ff057957f19d615311549b
Nikita-Oskin <91139480+Nikita-Oskin@users.noreply.github.com>
2022-06-05 19:29:10 +0300

    Update MainWindow.xaml.cs

--- Task_wpf/MainWindow.xaml.cs
+++ Task_wpf/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace pcshop_wpf
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
