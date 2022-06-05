ed5cf0bc243a4f66be9166fc0c46977e287294b4
Nikita-Oskin <91139480+Nikita-Oskin@users.noreply.github.com>
2022-06-05 18:42:12 +0300

    Add files via upload

---
+++ Task 2_3/Source/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string log = @"__________.__                 __   ________       ________  
+\______   \  |   ____   ____ |  | _\_____  \      \_____  \ 
+ |    |  _/  |  /  _ \_/ ___\|  |/ //  ____/        _(__  < 
+ |    |   \  |_(  <_> )  \___|    </       \       /       \
+ |______  /____/\____/ \___  >__|_ \_______ \_____/______  /
+        \/                 \/     \/       \/_____/      \/ ";
+
+            Console.WriteLine(log);
+
+            Console.ReadKey();
+        }
+    }
+}
